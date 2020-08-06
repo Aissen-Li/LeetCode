ddcdb48e2afa8e82315fc1757f0804bc5684af1e
liyifan <aissen_f@163.com>
2020-08-06 22:31:12 +0800

    8/6fromHome

---
+++ 1046.lastStoneWeight.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while len(stones) >= 2:
+            stones.sort()
+            stones.append(stones.pop()-stones.pop())
+        return stones[0]
